17b0c230801c688dd93e07a67f959137208f6f21
Dani-1138 <danieldemeke92@gmail.com>
2023-12-19 20:44:11 +0300

    mern stack music app responsiveness

--- client/src/components/rightside/search.js
+++ client/src/components/rightside/search.js
@@ -1,84 +1,80 @@
-import React, { useEffect, useState } from 'react';
-import { useNavigate } from 'react-router-dom';
+import React, { useEffect, useState } from "react";
+import { useNavigate } from "react-router-dom";
 
-import { FiSearch } from 'react-icons/fi';
-import '../../App.css'
-import styled from 'styled-components'
+import { FiSearch } from "react-icons/fi";
+import "../../App.css";
+import styled from "styled-components";
 import { setSearchAction } from "../../redux/action";
-import { useDispatch } from 'react-redux';
-import { device } from './responsive'
+import { useDispatch } from "react-redux";
+import { device } from "./responsive";
 
 const SearchDiv = styled.div`
-    display: flex;
-    align-items:center;
-    background: transparent;
-    background: rgba(54, 69, 98, 0.3);
-    width: 30%;
-    height: 2rem;
-    border-radius: 5px;
-    margin: 1rem 2.5rem;
-    box-shadow: 1px 1px 2px 1px 090f1f;
-    @media ${device.tablet} {
-      width: 50%;
-      margin: 1rem 4.5rem;
-    }
-    @media ${device.mobileL} {
-      width: 70%;
-     
-    }
-`
+  display: flex;
+  align-items: center;
+  background: transparent;
+  background: rgba(54, 69, 98, 0.3);
+  width: 30%;
+  height: 2rem;
+  border-radius: 5px;
+  margin: 1rem 2.5rem;
+  box-shadow: 1px 1px 2px 1px 090f1f;
+  @media ${device.tablet} {
+    width: 50%;
+    margin: 1rem 4.5rem;
+  }
+  @media ${device.mobileL} {
+    width: 70%;
+  }
+`;
 const InputLabel = styled.label.attrs({
-    for: 'search-field'
-})`
-  
-`
+  for: "search-field",
+})``;
 
 const SearchInput = styled.input.attrs({
-    type: 'search',
-    placeholder: 'Search',
-    id: 'search-field',
-    name: 'search-field'
+  type: "search",
+  placeholder: "Search",
+  id: "search-field",
+  name: "search-field",
 })`
-font-size: 14px;
-height: 2rem;
-width: 90%;
-padding: 1rem;
-background: transparent;
+  font-size: 14px;
+  height: 2rem;
+  width: 90%;
+  padding: 1rem;
+  background: transparent;
 
-border: none;
-border-radius: 5px;
-color: #fff;
+  border: none;
+  border-radius: 5px;
+  color: #fff;
 
-transition: margin 300ms cubic-bezier(0.645, 0.045, 0.355, 1);
+  transition: margin 300ms cubic-bezier(0.645, 0.045, 0.355, 1);
 
-&:focus,
-&:active {
-  outline: none;
-}
-&::placeholder {
-  color: rgb(90,90,90);
-}
-`
-
+  &:focus,
+  &:active {
+    outline: none;
+  }
+  &::placeholder {
+    color: rgb(90, 90, 90);
+  }
+`;
 
 const SearchSong = () => {
-  const dispatch = useDispatch()
+  const dispatch = useDispatch();
   const [searchTerm, setSearchTerm] = useState("");
 
-  useEffect(()=>{
+  useEffect(() => {
     dispatch(setSearchAction(searchTerm));
-  },[searchTerm])
+  }, [searchTerm]);
 
   return (
     <>
-        <SearchDiv>
-            <InputLabel>
-            <FiSearch className='search-icon'/>
-            </InputLabel>
-            <SearchInput onChange={(e) => setSearchTerm(e.target.value)}/>
-        </SearchDiv>
+      <SearchDiv>
+        <InputLabel>
+          <FiSearch className="search-icon" />
+        </InputLabel>
+        <SearchInput onChange={(e) => setSearchTerm(e.target.value)} />
+      </SearchDiv>
     </>
-  )
-}
+  );
+};
 
-export default SearchSong
+export default SearchSong;
--- client/src/components/rightside/widgetcard.js
+++ client/src/components/rightside/widgetcard.js
@@ -5,30 +5,28 @@ import { useDispatch, useSelector } from "react-redux";
 import { NavLink } from "react-router-dom";
 import styled from "styled-components";
 import { setAllSongs, setRecentSongs } from "../../redux/action";
-import { device } from './responsive'
+import { device } from "./responsive";
 
 export const WidgetWrapper = styled.div`
-    display: flex;
-    align-items: center;
-    height: 67%;
-    width: 100%;
-    justify-content: space-around;
-   
-    padding: 2rem 2rem;
-    padding: 2rem;
-    margin-bottom: 0;
-    @media ${device.mobileL} {
-display: none;
-      }
-      @media ${device.tablet} {
-        justify-content: center;
+  display: flex;
+  align-items: center;
+  height: 67%;
+  width: 100%;
+  justify-content: space-around;
 
-        margin-left: 4rem;
-      }
+  padding: 2rem 2rem;
+  padding: 2rem;
+  margin-bottom: 0;
+  @media ${device.mobileL} {
+    display: none;
+  }
+  @media ${device.tablet} {
+    justify-content: center;
 
-`
+    margin-left: 4rem;
+  }
+`;
 const Widget = styled.div`
-
   /* position: relative; */
   width: 100%;
   height: 100%;
@@ -44,31 +42,29 @@ const Widget = styled.div`
   overflow-x: scroll;
   cursor: pointer;
   &:hover {
-  transform: scale(1.08);
+    transform: scale(1.08);
   }
   @media ${device.tablet} {
     width: 95%;
-  height: 70%;
-  margin-left:0rem;
-      }
-      @media ${device.mobileL} {
-display: none;
-      }
+    height: 70%;
+    margin-left: 0rem;
+  }
+  @media ${device.mobileL} {
+    display: none;
+  }
 
-      overflow-y: scroll;
-  ::-webkit-scrollbar{
+  overflow-y: scroll;
+  ::-webkit-scrollbar {
     height: 0px;
     width: 0px;
   }
-  ::-webkit-scrollbar-track{
-    -webkit-box-shadow: inset 0 0 6px rgba(0,0,0,0.3);
-
+  ::-webkit-scrollbar-track {
+    -webkit-box-shadow: inset 0 0 6px rgba(0, 0, 0, 0.3);
   }
   ::-webkit-scrollbar-thumb {
     background-color: rgb(30, 30, 30);
-
   }
-`
+`;
 
 const WidgetTitle = styled.p`
   font-weight: 600;
@@ -78,11 +74,11 @@ const WidgetTitle = styled.p`
   text-overflow: ellipsis;
   white-space: nowrap;
   margin: 0 1rem;
-    `
+`;
 
 const MainEntry = styled.div`
-height: 80%;
-    `
+  height: 80%;
+`;
 
 const WidgetFade = styled.div`
   position: absolute;
@@ -103,134 +99,71 @@ const WidgetFade = styled.div`
   align-items: flex-end;
   justify-content: flex-end;
   padding: 8%;
-    `
-const WidgetButton = styled.div`
-        
-        `
+`;
+const WidgetButton = styled.div``;
 const EntryBody = styled.div`
   width: 100%;
   align-items: center;
   margin-top: 10%;
   display: flex;
   align-items: center;
-
-`
+`;
 const EntryImg = styled.img`
   height: 50px;
   width: 50px;
   border-radius: 15px;
   margin-right: 10px;
-`
+`;
 const EntryFlex = styled.div`
   flex-direction: column;
   justify-content: center;
   overflow-y: hidden;
-`
+`;
 const EntryTitle = styled.p`
-    color: #c9d0e3;
-    margin: 0px 0px 5px;
-    font-weight: 600;
+  color: #c9d0e3;
+  margin: 0px 0px 5px;
+  font-weight: 600;
   font-size: 1.125rem;
   overflow: hidden;
   text-overflow: ellipsis;
   white-space: nowrap;
-    `
+`;
 const EntrySubTitle = styled.p`
-    font-weight: 400;
-    font-size: 12px;
-    color: #c4d0e37c;
-    margin: 0;
-    overflow: hidden;
-    text-overflow: ellipsis;
-    white-space: nowrap;
-    `
+  font-weight: 400;
+  font-size: 12px;
+  color: #c4d0e37c;
+  margin: 0;
+  overflow: hidden;
+  text-overflow: ellipsis;
+  white-space: nowrap;
+`;
 const Nav = styled(NavLink)`
-          height: 100%;
-    width: 100%;
-    `
+  height: 100%;
+  width: 100%;
+`;
 export const Widgetcard1 = () => {
-  const dispatch = useDispatch()
-  const data = useSelector(state => state.recentSongReducer.allSong)
+  const dispatch = useDispatch();
+  const data = useSelector((state) => state.recentSongReducer.allSong);
 
   useEffect(() => {
-    dispatch(setRecentSongs())
-  }, [])
+    dispatch(setRecentSongs());
+  }, []);
 
   return (
     <>
-      <Nav to='get-recent' >
+      <Nav to="get-recent">
         <Widget>
           <WidgetTitle>New Released Songs</WidgetTitle>
           <MainEntry>
-            {data && data.map(song => (
-              <EntryBody>
-
-                <EntryImg className="bottom-image" src={song.imageURL} />
-
-                <EntryFlex>
-                  <EntryTitle>
-                    {song.name}
-                  </EntryTitle>
-                  <EntrySubTitle>
-                    {song.artist}
-                  </EntrySubTitle>
-                </EntryFlex>
-
-
-              </EntryBody>
-            ))}
-          </MainEntry>
-          {/* <WidgetFade>
-                    <WidgetButton>
-                        <FiChevronRight />
-                    </WidgetButton>
-                </WidgetFade> */}
-        </Widget>
-
-      </Nav>
-
-    </>
-  )
-}
-const FavoriteTrue = styled(AiFillHeart)`
-  font-weight: bold;
-  height: 32px;
-  width: 32px;
-  color: red;
-  float: right;
-  font-size: 30px;
-  margin-right: .3rem;
-`
-export const Widgetcard2 = () => {
-  const dispatch = useDispatch()
-  const data = useSelector(state => state.songsReducer.allSongs)
-
-  useEffect(()=> {
-    dispatch(setAllSongs())
-  },[])
-
-  return (
-    <>
-      <Nav to='favorite' >
-        <Widget>
-          <WidgetTitle>Favorite Songs<FavoriteTrue /></WidgetTitle>
-          <MainEntry>
-            {data && data.filter((data) =>
-              data.favorite === true).map(song => (
+            {data &&
+              data.map((song) => (
                 <EntryBody>
-
                   <EntryImg className="bottom-image" src={song.imageURL} />
 
                   <EntryFlex>
-                    <EntryTitle>
-                      {song.name}
-                    </EntryTitle>
-                    <EntrySubTitle>
-                      {song.artist}
-                    </EntrySubTitle>
+                    <EntryTitle>{song.name}</EntryTitle>
+                    <EntrySubTitle>{song.artist}</EntrySubTitle>
                   </EntryFlex>
-
-
                 </EntryBody>
               ))}
           </MainEntry>
@@ -240,44 +173,49 @@ export const Widgetcard2 = () => {
                     </WidgetButton>
                 </WidgetFade> */}
         </Widget>
-
       </Nav>
-
     </>
-  )
-}
-export const Widgetcard3 = () => {
-  const dispatch = useDispatch()
-  const data = useSelector(state => state.songsReducer.allSongs)
+  );
+};
+const FavoriteTrue = styled(AiFillHeart)`
+  font-weight: bold;
+  height: 32px;
+  width: 32px;
+  color: red;
+  float: right;
+  font-size: 30px;
+  margin-right: 0.3rem;
+`;
+export const Widgetcard2 = () => {
+  const dispatch = useDispatch();
+  const data = useSelector((state) => state.songsReducer.allSongs);
 
   useEffect(() => {
-    dispatch(setAllSongs())
-  }, [])
+    dispatch(setAllSongs());
+  }, []);
 
   return (
     <>
-      <Nav to='get-country-song' >
+      <Nav to="favorite">
         <Widget>
-          <WidgetTitle>Best Country Songs</WidgetTitle>
+          <WidgetTitle>
+            Favorite Songs
+            <FavoriteTrue />
+          </WidgetTitle>
           <MainEntry>
-            {data && data.filter((data) =>
-              data.category === 'Country').map(song => (
-              <EntryBody>
+            {data &&
+              data
+                .filter((data) => data.favorite === true)
+                .map((song) => (
+                  <EntryBody>
+                    <EntryImg className="bottom-image" src={song.imageURL} />
 
-                <EntryImg className="bottom-image" src={song.imageURL} />
-
-                <EntryFlex>
-                  <EntryTitle>
-                    {song.name}
-                  </EntryTitle>
-                  <EntrySubTitle>
-                    {song.artist}
-                  </EntrySubTitle>
-                </EntryFlex>
-
-
-              </EntryBody>
-            ))}
+                    <EntryFlex>
+                      <EntryTitle>{song.name}</EntryTitle>
+                      <EntrySubTitle>{song.artist}</EntrySubTitle>
+                    </EntryFlex>
+                  </EntryBody>
+                ))}
           </MainEntry>
           {/* <WidgetFade>
                     <WidgetButton>
@@ -285,14 +223,49 @@ export const Widgetcard3 = () => {
                     </WidgetButton>
                 </WidgetFade> */}
         </Widget>
-
       </Nav>
-
     </>
-  )
-}
-const Widget4 = styled.div`
+  );
+};
+export const Widgetcard3 = () => {
+  const dispatch = useDispatch();
+  const data = useSelector((state) => state.songsReducer.allSongs);
 
+  useEffect(() => {
+    dispatch(setAllSongs());
+  }, []);
+
+  return (
+    <>
+      <Nav to="get-country-song">
+        <Widget>
+          <WidgetTitle>Best Country Songs</WidgetTitle>
+          <MainEntry>
+            {data &&
+              data
+                .filter((data) => data.category === "Country")
+                .map((song) => (
+                  <EntryBody>
+                    <EntryImg className="bottom-image" src={song.imageURL} />
+
+                    <EntryFlex>
+                      <EntryTitle>{song.name}</EntryTitle>
+                      <EntrySubTitle>{song.artist}</EntrySubTitle>
+                    </EntryFlex>
+                  </EntryBody>
+                ))}
+          </MainEntry>
+          {/* <WidgetFade>
+                    <WidgetButton>
+                        <FiChevronRight />
+                    </WidgetButton>
+                </WidgetFade> */}
+        </Widget>
+      </Nav>
+    </>
+  );
+};
+const Widget4 = styled.div`
   /* position: relative; */
   width: 100%;
   height: 100%;
@@ -307,64 +280,55 @@ const Widget4 = styled.div`
   transition: all 0.2s ease-in-out;
   cursor: pointer;
   &:hover {
-  transform: scale(1.08);
+    transform: scale(1.08);
   }
   overflow-x: scroll;
   overflow-y: scroll;
-  ::-webkit-scrollbar{
+  ::-webkit-scrollbar {
     width: 0px;
   }
-  ::-webkit-scrollbar-track{
-    -webkit-box-shadow: inset 0 0 6px rgba(0,0,0,0.3);
-
+  ::-webkit-scrollbar-track {
+    -webkit-box-shadow: inset 0 0 6px rgba(0, 0, 0, 0.3);
   }
   ::-webkit-scrollbar-thumb {
     background-color: rgb(30, 30, 30);
-
   }
   @media ${device.tabletL} {
     display: none;
     width: 0%;
-      }
-      @media ${device.tabletL} {
-display: none;
-      }
-`
+  }
+  @media ${device.tabletL} {
+    display: none;
+  }
+`;
 export const Widgetcard4 = () => {
-  const dispatch = useDispatch()
-  const search = useSelector(state => state.searchReducer.searchTerm)
-  const data = useSelector(state => state.songsReducer.allSongs)
+  const dispatch = useDispatch();
+  const search = useSelector((state) => state.searchReducer.searchTerm);
+  const data = useSelector((state) => state.songsReducer.allSongs);
 
   useEffect(() => {
-    dispatch(setAllSongs())
-  }, [])
+    dispatch(setAllSongs());
+  }, []);
 
   return (
     <>
-      <Nav to='get-recent' >
+      <Nav to="get-recent">
         <Widget4>
-
           <WidgetTitle>Best Raggae songs</WidgetTitle>
           <MainEntry>
-            {data && data.filter((data) =>
-           data.category === 'Raggae'
-              ).map(song => (
-              <EntryBody>
+            {data &&
+              data
+                .filter((data) => data.category === "Raggae")
+                .map((song) => (
+                  <EntryBody>
+                    <EntryImg className="bottom-image" src={song.imageURL} />
 
-                <EntryImg className="bottom-image" src={song.imageURL} />
-
-                <EntryFlex>
-                  <EntryTitle>
-                    {song.name}
-                  </EntryTitle>
-                  <EntrySubTitle>
-                    {song.artist}
-                  </EntrySubTitle>
-                </EntryFlex>
-
-
-              </EntryBody>
-            ))}
+                    <EntryFlex>
+                      <EntryTitle>{song.name}</EntryTitle>
+                      <EntrySubTitle>{song.artist}</EntrySubTitle>
+                    </EntryFlex>
+                  </EntryBody>
+                ))}
           </MainEntry>
           {/* <WidgetFade>
                     <WidgetButton>
@@ -372,9 +336,7 @@ export const Widgetcard4 = () => {
                     </WidgetButton>
                 </WidgetFade> */}
         </Widget4>
-
       </Nav>
-
     </>
-  )
-}
+  );
+};
