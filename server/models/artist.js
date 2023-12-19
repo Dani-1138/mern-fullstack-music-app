ceb067318589aea0c5caa0bebf715a1c81d38672
Dani-1138 <danieldemeke92@gmail.com>
2023-12-19 20:38:46 +0300

    mern stack standardizatio

--- server/models/artist.js
+++ server/models/artist.js
@@ -1,25 +1,24 @@
 const mongoose = require("mongoose");
 
-const artistSchema =mongoose.Schema(
-    {
-        name : {
-            type: String,
-            required : true,
-        },
-        imageURL: {
-            type: String,
-            required : true,
-        },
-        twitter : {
-            type: String,
-            required : true,
-        },
-        instagram: {
-            type: String,
-            required : true,
-        },
-
+const artistSchema = mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: true,
     },
-    {timestamps: true}
+    imageURL: {
+      type: String,
+      required: true,
+    },
+    twitter: {
+      type: String,
+      required: true,
+    },
+    instagram: {
+      type: String,
+      required: true,
+    },
+  },
+  { timestamps: true }
 );
-module.exports = mongoose.model("artist", artistSchema)
+module.exports = mongoose.model("artist", artistSchema);
--- server/models/song.js
+++ server/models/song.js
@@ -1,40 +1,39 @@
 const mongoose = require("mongoose");
 
-const songSchema =mongoose.Schema(
-    {
-        name : {
-            type: String,
-            required : true,
-        },
-        imageURL: {
-            type: String,
-            required : false,
-        },
-        songUrl: {
-            type: String,
-            required : false,
-        },
-        album: {
-            type: String,
-        },
-        artist: {
-            type: String,
-            required : true,
-        },
-       language: {
-            type: String,
-            required : true,
-        },
-        category: {
-            type: String,
-            required : true,
-        },
-        favorite: {
-            type: Boolean,
-            default: false,
-        },
-
+const songSchema = mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: true,
     },
-    {timestamps: true}
+    imageURL: {
+      type: String,
+      required: false,
+    },
+    songUrl: {
+      type: String,
+      required: false,
+    },
+    album: {
+      type: String,
+    },
+    artist: {
+      type: String,
+      required: true,
+    },
+    language: {
+      type: String,
+      required: true,
+    },
+    category: {
+      type: String,
+      required: true,
+    },
+    favorite: {
+      type: Boolean,
+      default: false,
+    },
+  },
+  { timestamps: true }
 );
-module.exports = mongoose.model("song", songSchema)
+module.exports = mongoose.model("song", songSchema);
