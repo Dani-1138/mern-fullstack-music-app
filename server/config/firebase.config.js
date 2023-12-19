d22d23be4f3c7fa01161a462c810d848f308e6c9
Dani-1138 <danieldemeke92@gmail.com>
2023-12-19 20:39:24 +0300

    mern stack standardizatio

--- server/config/firebase.config.js
+++ server/config/firebase.config.js
@@ -1,10 +1,9 @@
-
 var admin = require("firebase-admin");
 
 var serviceAccount = require("./serviceAccountKey.json");
 
 admin.initializeApp({
-  credential: admin.credential.cert(serviceAccount)
+  credential: admin.credential.cert(serviceAccount),
 });
 
 module.exports = admin;
